4c5afc5283b30e6e51eba83fb6a175454db630b0
John Reilly <johnny_reilly@hotmail.com>
2018-09-07 17:06:47 +0100

    debugging in vscode for server

--- src/server/server/index.ts
+++ src/server/server/index.ts
@@ -7,19 +7,26 @@ import { config } from './config';
 import { logger } from './logging';
 // import { routes } from "./routes";
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 const app = new Koa();
 
 app.use(logger);
 // app.use(routes);
 
-const publicPath = path.join(__dirname, '..', 'client', 'dist');
+const publicPath = isDevelopment
+    ? path.join(__dirname, '..', '..', 'client', 'dist')
+    : path.join(__dirname, '..', 'client', 'dist');
+
 const indexHtmlPath = path.join(publicPath, 'index.html');
 app.use(serve(publicPath));
-app.use(async (ctx) => {
+app.use(async ctx => {
     await send(ctx, 'index.html', { root: publicPath });
 });
 
 app.listen(config.port);
 
 // tslint:disable-next-line:max-line-length
-console.log(`Server running on port ${config.port}; static files served from ${publicPath}, SPA template from ${indexHtmlPath}`);
+console.log(
+    `Server running on port ${config.port}; static files served from ${publicPath}, SPA template from ${indexHtmlPath}`
+);
