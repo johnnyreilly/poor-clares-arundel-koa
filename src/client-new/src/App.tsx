7afc104f86c79d97f3e21422496d0bf2195c59e4
John Reilly <johnny_reilly@hotmail.com>
2018-10-10 06:24:26 +0100

    Add react-router

--- src/client-new/src/App.tsx
+++ src/client-new/src/App.tsx
@@ -1,59 +1,13 @@
 import * as React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import './App.css';
-
-import house from './images/house.jpg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home } from './Home';
 
 export class App extends React.Component {
     public render() {
         return (
-            <div className="text-center">
-                <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-                    <header className="masthead mb-auto">
-                        <div className="inner">
-                            <h2 className="masthead-brand">Welcome to the Poor Clares of Arundel</h2>
-                        </div>
-                    </header>
-
-                    <main role="main" className="inner cover">
-                        <a href="/us" title="Learn more about us...">
-                            <img
-                                id="house"
-                                src={house}
-                                alt="Click here to enter..."
-                                className="img-responsive img-rounded"
-                            />
-                        </a>
-                        <p className="lead">
-                            We are sisters, who share prayer, work, laughter and struggles,
-                            <br />
-                            and live according to the Form of Life drawn up by St Clare of Assisi in 1253.
-                        </p>
-                        <p className="lead">
-                            <a href="/us">Learn more about us...</a>
-                        </p>
-                    </main>
-
-                    <footer className="mastfoot mt-auto">
-                        <div className="inner">
-                            <p>
-                                © Community of Poor Clares, Arundel 2014
-                                <br /> Convent of Poor Clares, Crossbush, Arundel, BN18 9PJ
-                            </p>
-                            <p>
-                                <a href="mailto:poorclarescrossbusharundel@hotmail.co.uk">
-                                    <FontAwesomeIcon icon={faEnvelope} /> poorclarescrossbusharundel@hotmail.co.uk
-                                </a>{' '}
-                                <a href="https://www.facebook.com/poorclaresarundel">
-                                    <FontAwesomeIcon icon={faFacebook} /> Facebook
-                                </a>
-                            </p>
-                        </div>
-                    </footer>
-                </div>
-            </div>
+            <Router>
+                <Route exact path="/" component={Home} />
+            </Router>
         );
     }
 }
