167a1be3575007c046a044e0a9499192f4cff9b1
John Reilly <johnny_reilly@hotmail.com>
2018-10-21 07:35:16 +0100

    remove unneeded ui

--- src/client-new/src/Main/index.tsx
+++ src/client-new/src/Main/index.tsx
@@ -7,14 +7,10 @@ type Props = RouteComponentProps<{}>;
 export class Main extends React.Component<Props> {
     public render() {
         return (
-            <div>
-            {this.props.match.path}
-                <p>Main component</p>
-                <Switch>
-                    <Route path="/us" component={Us} />
-                    <Redirect to="/" />
-                </Switch>
-            </div>
+            <Switch>
+                <Route path="/us" component={Us} />
+                <Redirect to="/" />
+            </Switch>
         );
     }
 }
