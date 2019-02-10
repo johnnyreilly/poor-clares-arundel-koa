0602dbf33d3b72706f74ec2a6cd0e3d96d0b52ea
John Reilly <johnny_reilly@hotmail.com>
2019-02-10 18:29:05 +0000

    add a nice loading icon

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSnowflake } from '@fortawesome/free-solid-svg-icons';
 import { Home } from './Home';
 const Main = React.lazy(() => import('./main'));
 const TheConvent = React.lazy(() => import('./the-convent'));
 
 const theConventPath = 'the-convent';
+
+const Loading: React.FunctionComponent = _ => (
+    <h1 style={{ textAlign: 'center' }}>
+        <FontAwesomeIcon icon={faSnowflake} spin /> Loading...
+    </h1>
+);
 
 export class App extends React.Component {
     public render() {
@@ -14,7 +22,7 @@ export class App extends React.Component {
                 <Route
                     path={`/${theConventPath}`}
                     render={props => (
-                        <React.Suspense fallback={<div>Loading...</div>}>
+                        <React.Suspense fallback={<Loading />}>
                             <TheConvent {...props} />
                         </React.Suspense>
                     )}
@@ -22,7 +30,7 @@ export class App extends React.Component {
                 <Route
                     path="/"
                     render={props => (
-                        <React.Suspense fallback={<div>Loading...</div>}>
+                        <React.Suspense fallback={<Loading />}>
                             <Main {...props} />
                         </React.Suspense>
                     )}
