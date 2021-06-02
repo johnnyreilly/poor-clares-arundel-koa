8b9d2084ded3fb07e4e4a44a95c81aba5f2a1fb5
John Reilly <johnny_reilly@hotmail.com>
2021-06-02 11:49:03 +0100

    refresh lockfile (#47)
    * refresh lockfile
    
    * compiles
    
    * tslint -> eslint
    
    * bash
    
    * come on github
    
    * no tslint

--- src/server/server/index.ts
+++ src/server/server/index.ts
@@ -12,12 +12,16 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 
 const app = new Koa();
 
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-      // tslint:disable-next-line:quotemark
-      scriptSrc: ["'self'", "'unsafe-inline'", "storage.googleapis.com", "www.google-analytics.com"],
-    }
-  }));
+app.use(
+    helmet.contentSecurityPolicy(
+        {
+            useDefaults: true,
+            directives: {
+                scriptSrc: ["'self'", "'unsafe-inline'", 'storage.googleapis.com', 'www.google-analytics.com'],
+            },
+        } as any // see https://github.com/venables/koa-helmet/pull/69
+    )
+);
 app.use(logger);
 app.use(routes);
 
@@ -27,13 +31,12 @@ const publicPath = isDevelopment
 
 const indexHtmlPath = path.join(publicPath, 'index.html');
 app.use(serve(publicPath));
-app.use(async ctx => {
+app.use(async (ctx) => {
     await send(ctx, 'index.html', { root: publicPath });
 });
 
 app.listen(config.port);
 
-// tslint:disable-next-line:max-line-length
 console.log(
     `Server running on port ${config.port}; static files served from ${publicPath}, SPA template from ${indexHtmlPath}`
 );
